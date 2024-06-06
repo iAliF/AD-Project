2cf848e34c79225f76df8c2ba40dec4a54bca7cb
Ali <the.alif.dev@gmail.com>
2024-06-06 18:30:29 +0330

    Added Node class

---
+++ Project-1/Node.cs
@@ -0,0 +1,18 @@
+﻿namespace Project_1
+{
+    public class Node
+    {
+        public readonly int Key;
+        public Node Left;
+        public Node Parent;
+        public Node Right;
+
+        public Node(int key = 0, Node parent = null, Node left = null, Node right = null)
+        {
+            Key = key;
+            Parent = parent;
+            Left = left;
+            Right = right;
+        }
+    }
+}
